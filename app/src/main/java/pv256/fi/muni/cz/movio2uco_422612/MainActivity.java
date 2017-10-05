43e05931098426a00d3ce747df709fb0969394a1
Patrik Cyprian <422612@mail.muni.cz>
2017-10-05 18:55:56 +0200

    poject init

---
+++ app/src/main/java/pv256/fi/muni/cz/movio2uco_422612/MainActivity.java
@@ -0,0 +1,13 @@
+package pv256.fi.muni.cz.movio2uco_422612;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/pv256/fi/muni/cz/movio2uco_422612/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package pv256.fi.muni.cz.movio2uco_422612;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
